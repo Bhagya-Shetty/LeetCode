320edc6082cf4d324b014f4bd9c88a0b1919fbd8
Bhagya-Shetty <shetty21bhagya@gmail.com>
2025-10-01 23:46:19 +0530

    Time: 2 ms (11.30%) | Memory: 45.9 MB (6.18%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -2,19 +2,12 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> answer=new ArrayList<>();
         for(int i=1;i<=n;i++){
-            if(i%3==0&&i%5==0){
-                answer.add("FizzBuzz");
+            StringBuilder sb=new StringBuilder();
+            if(i%3==0) sb.append("Fizz");
+            if(i%5==0) sb.append("Buzz");
+            if(sb.length()==0) sb.append(String.valueOf(i));
+            answer.add(sb.toString());
             }
-            else if(i%3==0){
-                answer.add("Fizz");
-            }
-            else if(i%5==0){
-                answer.add("Buzz");
-            }
-            else{
-                answer.add(String.valueOf(i));
-            }
-    }
     return answer;
 }
 }
