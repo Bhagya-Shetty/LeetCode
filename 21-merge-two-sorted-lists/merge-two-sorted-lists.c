83887a3f08dad91f76fe674fc52dc48756e5a51f
Bhagya-Shetty <shetty21bhagya@gmail.com>
2025-10-06 14:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (96.82%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.c
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.c
@@ -8,34 +8,32 @@
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     if(list1==0) return list2;
     if(list2==0) return list1;
-    struct ListNode*temp1=list1;
-    struct ListNode*temp2=list2;
-    struct ListNode*last=NULL;
-    if(temp1->val<=temp2->val){
-        last=list1;
-        temp1=temp1->next;
+    struct ListNode*head=NULL,*last=NULL;
+    if(list1->val<=list2->val){
+        head=last=list1;
+        list1=list1->next;
     }
     else{
-        last=list2;
-        temp2=temp2->next;
+        head=last=list2;
+        list2=list2->next;
     }
-    while(temp1!=0&&temp2!=0){
-        if(temp1->val<=temp2->val){
-            last->next=temp1;
-            last=temp1;
-            temp1=temp1->next;
+    while(list1!=0&&list2!=0){
+        if(list1->val<=list2->val){
+            last->next=list1;
+            last=list1;
+            list1=list1->next;
         }
         else{
-            last->next=temp2;
-            last=temp2;
-            temp2=temp2->next;
+            last->next=list2;
+            last=list2;
+            list2=list2->next;
         }
     }
-    if(temp1==0){
-        last->next=temp2;
+    if(list1==0){
+        last->next=list2;
     }
-    if(temp2==0){
-        last->next=temp1;
+    if(list2==0){
+        last->next=list1;
     }
-    return (list1->val<=list2->val)? list1:list2;
+    return head;
 }
