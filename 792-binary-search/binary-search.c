29248c758fa5ad8e016ccd30ca0853bf8d3a317f
Bhagya-Shetty <shetty21bhagya@gmail.com>
2025-10-08 14:03:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (4.01%) - LeetSync

--- 792-binary-search/binary-search.c
+++ 792-binary-search/binary-search.c
@@ -2,7 +2,7 @@ int search(int* nums,int numsSize,int target) {
     int low=0;
     int high=numsSize-1;
     while(low<=high){
-        int mid=(low+high)/2;
+        int mid=low+(high-low)/2;
         if(nums[mid]==target){
             return mid;
         }
