f9eb90d8fde4b5ffefb44800468ae5a0be68ecea
Bhagya-Shetty <shetty21bhagya@gmail.com>
2025-10-09 15:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (19.21%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -3,10 +3,6 @@ class Solution {
         if(n==0||n==1){
             return n;
         }
-    //     else{
-    //         return fib(n-1)+fib(n-2);
-    //     } 
-    // }
     int a=0;
     int b=1;
     int c=0;
