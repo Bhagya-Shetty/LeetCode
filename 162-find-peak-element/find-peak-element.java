077ef387bc67a084f85e083b07ec12ad8eae9eba
Bhagya-Shetty <shetty21bhagya@gmail.com>
2025-10-16 08:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (57.35%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,14 +1,12 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return 0;
-        if (nums[0] > nums[1]) return 0;
-        if (n == 2 && nums[0] < nums[1]) return 1;
-        for (int i = 1; i < n-1; i++){
-            if (nums[i] > nums[i+1] && nums[i] > nums[i-1]){
+        int n=nums.length;
+        if(n==1) return 0;
+        for(int i=1;i<n-1;i++){
+            if(nums[i]>nums[i-1]&&nums[i]>nums[i+1]){
                 return i;
             }
         }
-        return n-1;
+        return nums[0]>nums[1]?0:n-1;
     }
 }
