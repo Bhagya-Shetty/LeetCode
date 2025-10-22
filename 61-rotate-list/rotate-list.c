26f435a410f1630df961d2bd24b7743d6ddbd204
Bhagya-Shetty <shetty21bhagya@gmail.com>
2025-10-22 23:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (4.24%) - LeetSync

--- 61-rotate-list/rotate-list.c
+++ 61-rotate-list/rotate-list.c
@@ -6,23 +6,17 @@
  * };
  */
 struct ListNode* rotateRight(struct ListNode* head, int k) {
+    if(!head||!head->next||k==0) return head;
     struct ListNode*temp=head;
-    int count=0,i=0;
-    while(temp!=0){
+    int count=1,i=0;
+    while(temp->next!=0){
         temp=temp->next;
         count++;
     }
-    if(count==0) return NULL;
     k%=count;
     if(k==0) return head;
-    temp=head;
-    while(i<count-1){
-        temp=temp->next;
-        i++;
-    }
     temp->next=head;
     temp=head;
-    i=0;
     while(i<(count-k-1)){
         temp=temp->next;
         i++;
