697d66ef0340200a56ff67ac9f48710ba17e28c2
Bhagya-Shetty <shetty21bhagya@gmail.com>
2026-01-07 21:37:54 +0530

    Time: 2 ms (99.05%) | Memory: 72.5 MB (47.80%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -5,19 +5,27 @@ class Solution {
         for(int i=0;i<b;i++){
             if(bills[i]==5) five++;
             else if(bills[i]==10){
-                ten++;
-                five--;
-            }
-            else{
-                if(ten>0){
-                    ten--;
+                if(five>0){
+                    ten++;
                     five--;
                 }
-                else{
-                    five-=3;
-                }
+                else return false;
             }
-            if(ten<0||five<0) return false;
+            else{
+                if(five>0){
+                    if(ten>0){
+                        ten--;
+                        five--;
+                    }
+                    else{
+                        if(five>=3){
+                            five-=3;
+                        }
+                        else return false;
+                    }
+                }
+                else return false;
+            }
         }
         return true;
     }
